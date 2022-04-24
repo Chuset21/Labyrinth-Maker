304f76391b92501b09744dea1f4090df1999624f
Miguel <miguelarrieta02@gmail.com>
2022-04-24 15:18:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Labyrinth Maker
+## Goals
+- [x] Learn basics of Rust
+- [ ] Represent board in code
+- [ ] Print board to screen in a readable format
+- [ ] Devise algorithm to create a board with barriers
+- [ ] Create app or website to show board
+- [ ] Add functionality to website to create new board
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+
+
+/*
+Board looks like:
+
+\|   |   |   |   |   |   \|
+   -   -   -   -   -   -
+\|   |   |   |   |   |   \|
+   -   -   -   -   -   -
+\|   |   |   |   |   |   \|
+   -   -   -   -   -   -
+\|   |   |   |   |   |   \|
+   -   -   -   -   -   -
+\|   |   |   |   |   |   \|
+   -   -   -   -   -   -
+\|   |   |   |   |   |   \|
+
+Maximum of 24 barriers can be placed
+'\|' denotes the edge of the board where no barrier can be placed
+ */
